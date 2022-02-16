d3524ab1d2575a1aa0fca257bb4e36af94b28025
Salman <you@example.com>
2022-02-16 14:33:04 +0600

    login page

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// Login Page 
+document.getElementById('submit-btn').addEventListener('click', function () {
+    const inputEmail = document.getElementById('inputEmail');
+     const emailValue = inputEmail.value;
+
+     const inputPassword = document.getElementById('inputPassword');
+     const passwordValue = inputPassword.value;
+
+     if (emailValue == 'khelafate@islam.com' && passwordValue == 'islamia') {
+         window.location.href = 'banking.html'
+     }
+})
